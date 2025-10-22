0c9a0ca9f746def9d185e0f0a109c6c0107c4477
jaquellineh <164918839+jaquellineh@users.noreply.github.com>
2025-10-22 14:49:12 -0300

    atualizar

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-        
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
