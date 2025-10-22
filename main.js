bb4c52c7d823992901a41ff238e7fa6fbc962af3
jaquellineh <164918839+jaquellineh@users.noreply.github.com>
2025-10-22 15:03:46 -0300

    atualizar

--- main.js
+++ main.js
@@ -11,3 +11,8 @@
         textos[i].classList.add("ativo");
     }
 }
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:oo");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
